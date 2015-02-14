5d0c8a6d76a85c67ed19be0c5ba46a307073badb
Dylan <dylanzingler@gmail.com>
2015-02-14 17:39:26 -0500

    working on project

--- Projects/01/p1.c
+++ Projects/01/p1.c
@@ -6,9 +6,10 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 /*  Global constants  */
-#define MAX_LINE           (1000)
+#define MAX_LINE           (2000)
 
 /***
 Author: Dylan Zingler
@@ -28,6 +29,7 @@ Then a message must be piped to the function to echo it
 /*  Function declarations  */
 ssize_t Readline(int fd, void *vptr, size_t maxlen);
 ssize_t Writeline(int sockd, const void *vptr, size_t n);
+ssize_t ReadSock(int sockd, void *vptr);
 
 /*  Read a line from a socket  */
 ssize_t Readline(int sockd, void *vptr, size_t maxlen) {
@@ -37,28 +39,87 @@ ssize_t Readline(int sockd, void *vptr, size_t maxlen) {
     buffer = vptr;
 
     for ( n = 1; n < maxlen; n++ ) {
-	
-	if ( (rc = read(sockd, &c, 1)) == 1 ) {
-	    *buffer++ = c;
-	    if ( c == '\n' )
-		break;
-	}
-	else if ( rc == 0 ) {
-	    if ( n == 1 )
-		return 0;
-	    else
-		break;
-	}
-	else {
-	    if ( errno == EINTR )
-		continue;
-	    return -1;
-	}
+	    if ( (rc = read(sockd, &c, 1)) == 1 ) {
+	        *buffer++ = c;
+    	    if ( c == EOF )
+	    	    break;
+	    } else if ( rc == 0 ) {
+	        if ( n == 1 )
+	    	    return 0;
+	        else
+	    	    break;
+	    } else {
+	        if ( errno == EINTR )
+	    	    continue;
+	        return -1;
+	    }
     }
 
     *buffer = 0;
     return n;
 }
+
+
+ssize_t ReadSock(int sockd, void *vptr) {
+    ssize_t n, rc;
+    char    c, *buffer;
+    
+    buffer = vptr;
+    n = 1;        
+    
+    /* Good loop */
+    while (1){
+        rc = read(sockd, &c, 1);
+        printf("got here");        
+        /* Read Value was Good   */
+        if (rc == 1){
+            *buffer++ = c;
+            
+            if ( c == EOF )
+	    	    break;
+        /* Read Value was zero, server done sending message */	    	    
+        } else if (rc == 0){
+            if ( n == 1 )
+	    	    return 0;
+	        else
+	    	    break;
+	    	    
+	    /* Read Value stated that there was an error */
+        } else {
+	        if ( errno == EINTR )
+	    	    continue;
+	        return -1;
+	    }
+	    n++;          
+    }
+    
+    
+    
+    *buffer = 0;
+    return n;
+
+}
+
+ssize_t SockReader(int sockd, void *vptr){
+    /*ssize_t n, rc;*/
+    char    c, *buffer;
+    int count = 0;
+    buffer = vptr;
+
+
+    int len = 0;
+    ioctl(sockd, FIONREAD, &len);
+    if (len > 0) {
+        len = read(sockd, &c, 1);
+        *buffer++ = c;
+        count += 1;
+    }
+    
+    printf("LENGTH OF SOCKET DATA IS: %d", count);
+    return len;
+}
+
+
 
 
 /*  Write a line to a socket  */
@@ -152,15 +213,17 @@ int main (int argc, char *argv[]) {
     
     /* Send string to echo server, and retrieve response */
     Writeline(conn_s, buffer, strlen(buffer));
-    Readline(conn_s, buffer, MAX_LINE-1);
+/*    Readline(conn_s, buffer, MAX_LINE-1);*/
+    ReadSock(conn_s, buffer);
+/*    SockReader(conn_s, buffer);*/
     
     /* Output echoed String */
     printf("Echo Response: %s\n", buffer);
     
     if (shutdown(conn_s, 2) == 0){
-        printf("shutdown successful")
+        printf("shutdown successful");
     } else{
-        printf("Shutdown unsuccessful")
+        printf("Shutdown unsuccessful");
         }
     
     return EXIT_SUCCESS;
